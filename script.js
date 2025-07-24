b7c4d7800008fd68c70b392dd1517483cb2c7b11
Taraka Rama Reddy Karri <ktarakaramareddy@gmail.com>
2025-07-24 09:23:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const pickNumberBtn = document.getElementById("pickNumberBtn");
+const resultBtn = document.getElementById("result");
+
+const gifts = [
+  "₹100 Cash",
+  "Toy Car",
+  "Chocolate Box",
+  "₹500 Cash",
+  "Smartphone Cover",
+  "Book",
+  "Headphones",
+  "₹50 Cash",
+  "Gift Voucher",
+  "Watch",
+  "Teddy Bear",
+  "Bluetooth Speaker",
+  "Movie Ticket",
+  "₹200 Cash",
+  "Puzzle Game",
+  "Perfume",
+  "Sunglasses",
+  "₹1000 Cash",
+  "Board Game",
+  "Fitness Band",
+  "Digital Clock",
+  "Lamp",
+  "Shopping Voucher",
+  "Laptop Bag",
+  "Wireless Mouse",
+  "Travel Mug",
+  "Notebook Set",
+  "Gaming Mousepad",
+  "₹250 Cash",
+  "Keychain",
+  "Water Bottle",
+  "Portable Charger",
+  "Desk Organizer",
+  "Cooking Set",
+  "Action Figure",
+  "₹300 Cash",
+  "Travel Pillow",
+  "Mini Backpack",
+  "Personalized Mug",
+  "Gaming Controller",
+  "Camera Strap",
+  "Toy Robot",
+  "₹750 Cash",
+  "Sports Equipment",
+  "Pen Set",
+  "Bluetooth Earbuds",
+  "Digital Photo Frame",
+  "Wallet",
+  "Backpack",
+  "Gift Hamper",
+];
+console.log(gifts);
+pickNumberBtn.addEventListener("click", function () {
+  let randomNum = Math.random() * 50;
+  let drawnNumber = Math.floor(randomNum) + 1;
+  console.log(gifts[drawnNumber - 1]);
+  const gift = gifts[drawnNumber - 1];
+  resultBtn.textContent = `You have got ${drawnNumber}, and you won ${gift}`;
+});
